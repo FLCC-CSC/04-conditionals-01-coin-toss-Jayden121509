04323127aa0fc0d97c00ca5634fb8cb1a49f239c
Jayden <257486807+Jayden121509@users.noreply.github.com>
2026-03-01 20:02:07 +0000

    coin flip

--- coin_toss.py
+++ coin_toss.py
@@ -1,23 +1,38 @@
 # FILE NAME - coin_toss.py
-# NAME: 
-# DATE: 
-# BRIEF DESCRIPTION:  
+
+# NAME: Jayden Blair
+# DATE: 3/1/26
+# BRIEF DESCRIPTION:  Create a coin toss program that randomly generates either heads or tails.
+
+
 # 1. Make sure you fill out the comments above
 # 2. Write your code in the proper spot
 # 3. Be sure to answer the Reflection Questions and Attestation below
 # 4. The Sample Output has been included in this code for your convenience
+
+
+
 ########## ENTER YER CODE BELOW THIS LINE ##########
 
+# Don't forget to import random!!!!!
 
+import random
 
+print("===== Coin Flipper =====")
 
-
-
+flip = random.randint(0, 1)
+if flip == 0:
+    print("Heads")
+else:
+    print("Tails")
 
 
 
 
 ########### END YER CODE ABOVE THIS LINE ###########
+
+    
+
 
 
 ########################################
@@ -44,24 +59,10 @@ Tails
 
 1. What was the hardest part of completing this lab? 
 
+The else statement was a little tricky to figure out, but once I got it, I understood how it worked.
 
 
 
 
 
-
-'''
-
-########################################
-#            ATTESTATION
-########################################
-'''
-It is critical in this class that you understand the concepts as we explore them because
-those concepts are required understanding for entry level programming. Reliance on resources
-like AI and internet sites like Chegg, CourseHero, StackOverflow, and general Google results
-may impede your understanding. Please rate how well you understand the concepts in this lab: 
-[ ] I understand very little about this lab.
-[ ] I am about 50/50 on this lab; I get parts of it but not the whole picture.
-[ ] I pretty much get it.
-[ ] I'm solid. Totally got it.
 '''
